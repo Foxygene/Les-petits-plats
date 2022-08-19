86658d9c1f9f38bf9e3625fa0dcc46006bd49ee7
Foxygene <foxygene.dev@gmail.com>
2022-08-19 21:16:42 +0200

    feat(search box): added to index.html flow

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { header } from './components/header/header';
+import { searchBox } from './components/search_box/searchbox';
 import { recipes } from './data/recipes.js';
 
 document.body.innerHTML = `<main">
   ${header()}
+  ${searchBox()}
 </main>`;
