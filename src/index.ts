fcd166db748089163d6636cb9b926c5f88828ef8
Foxygene <foxygene.dev@gmail.com>
2022-08-20 02:27:48 +0200

    feat(dropdown search): added to index.html flow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+import { dropdownSearch } from './components/dropdown_search/dropdownsearch';
 import { header } from './components/header/header';
 import { searchBox } from './components/search_box/searchbox';
 import { recipes } from './data/recipes.js';
 
-document.body.innerHTML = `<main">
+document.body.innerHTML =
+  /*html*/
+  `<main>
   ${header()}
   ${searchBox()}
+  <div class="dropdowns_container">
+  ${dropdownSearch()}
+  ${dropdownSearch('Appareils', 'bg_green')}
+  ${dropdownSearch('Ustensiles', 'bg_red')}
+  </div>
 </main>`;
