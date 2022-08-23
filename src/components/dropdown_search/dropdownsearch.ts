665040152840b1cf8f8dadca2cf91179c4e05cc9
Foxygene <foxygene.dev@gmail.com>
2022-08-23 23:49:30 +0200

    feat(dropdowns): added options

--- src/components/dropdown_search/dropdownsearch.ts
+++ src/components/dropdown_search/dropdownsearch.ts
@@ -1,9 +1,19 @@
-export const dropdownSearch = (placeholder = 'Ingredients', color = 'bg_blue', src = './arrowdownicon.svg'): string =>
+export const dropdownSearch = (
+  placeholder = 'Items',
+  color = 'bg_blue',
+  options: [string],
+  src = './arrowdownicon.svg'
+): string =>
   /*html*/
   `<div class="dropdown_search ${color}">
     <input placeholder="${placeholder}">
     <img src="${src}">
   </div>
   <div class="dropdown_options">
-    
+    ${options
+      .map((item) => {
+        return /*html*/ `
+      <a>${item}</a>`;
+      })
+      .join('')}}
   </div>`;
