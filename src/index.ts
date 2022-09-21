fba6c3f0108734830341072e7a7b8a07249cde08
Foxygene <foxygene.dev@gmail.com>
2022-09-21 19:45:34 +0200

    chore(getnoduplicate): fix to last commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { header } from './components/header/header';
 import { recipesCard } from './components/recipes_card/recipescard';
 import { searchBox } from './components/search_box/searchbox';
 import { recipes } from './data/recipes';
-import { getNoDuplicate } from './utils';
+import { getNoDuplicate } from './getnoduplicate';
 
 const ingredientList = getNoDuplicate('ingredient', recipes);
 const applianceList = getNoDuplicate('appliance', recipes);
