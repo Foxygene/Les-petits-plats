49a8198ef684c45b721e2c5e9559e64b4c9a5fb5
Foxygene <foxygene.dev@gmail.com>
2022-10-07 18:08:43 +0200

    chore(filter pill): event listener set to one time

--- src/components/filter_pill/use_pill.ts
+++ src/components/filter_pill/use_pill.ts
@@ -2,6 +2,6 @@ export const usePill = (filterBox: HTMLDivElement): void => {
   const deleteButtonList = filterBox.querySelectorAll<HTMLButtonElement>('.close_pill_button');
 
   deleteButtonList.forEach((deleteButton) => {
-    deleteButton.addEventListener('click', () => deleteButton.parentElement?.remove());
+    deleteButton.addEventListener('click', () => deleteButton.parentElement?.remove(), true);
   });
 };
