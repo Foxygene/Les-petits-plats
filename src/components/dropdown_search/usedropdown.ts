2c4a0e10f39f9abcd9794c9d4001cc5a383fd310
Foxygene <foxygene.dev@gmail.com>
2022-10-09 01:56:41 +0200

    feat(dropdown interaction): now fonctionnal dropdown options click

---
+++ src/components/dropdown_search/usedropdown.ts
@@ -0,0 +1,14 @@
+import { filterPill } from '../filter_pill/filter_pill';
+import { usePill } from '../filter_pill/use_pill';
+
+export const useDropdown = (filterBox: HTMLDivElement): void => {
+  const options = document.querySelectorAll('.option');
+
+  options.forEach((option) => {
+    option.addEventListener('click', () => {
+      console.log(option.innerHTML);
+      filterBox.innerHTML = filterBox.innerHTML + filterPill(option.innerHTML, option.parentElement?.classList[1]);
+      usePill(document.querySelector<HTMLDivElement>('.filter_box')!);
+    });
+  });
+};
