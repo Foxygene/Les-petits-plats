43968f642a82c9dc5cf86072c6b89e707093eeee
Foxygene <foxygene.dev@gmail.com>
2022-10-16 08:18:16 +0200

    feat(filter pill): closing now updated filter status

--- src/components/filter_pill/use_pill.ts
+++ src/components/filter_pill/use_pill.ts
@@ -1,7 +1,16 @@
+import { refreshDOMelementsToFilter } from '../../scripts/refreshdomelementstofilter';
+
 export const usePill = (filterBox: HTMLDivElement): void => {
   const deleteButtonList = filterBox.querySelectorAll<HTMLButtonElement>('.close_pill_button');
 
   deleteButtonList.forEach((deleteButton) => {
-    deleteButton.addEventListener('click', () => deleteButton.parentElement?.remove(), true);
+    deleteButton.addEventListener(
+      'click',
+      () => {
+        deleteButton.parentElement?.remove();
+        refreshDOMelementsToFilter();
+      },
+      true
+    );
   });
 };
