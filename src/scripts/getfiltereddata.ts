1c8206a5715ac80ec480b1980b23b9e05354059c
Foxygene <foxygene.dev@gmail.com>
2022-10-16 08:19:21 +0200

    feat(data filtering): WIP

--- src/scripts/getfiltereddata.ts
+++ src/scripts/getfiltereddata.ts
@@ -5,12 +5,22 @@ import type { Recipe } from '../type/recipes';
 export const filterRecipes = (activeFilters: filterListByType): Recipe[] => {
   const [ingredientsFilter, applianceFilter, ustensilsFilter] = activeFilters;
 
-  return recipes
-    .filter((recipe) => applianceFilter.includes(recipe.appliance))
-    .filter((recipe) => ustensilsFilter.some((ustensil) => recipe.ustensils.includes(ustensil)))
-    .filter((recipe) =>
+  let updatedRecipes = recipes;
+
+  if (applianceFilter.length !== 0)
+    updatedRecipes = updatedRecipes.filter((recipe) => applianceFilter.includes(recipe.appliance));
+
+  if (ustensilsFilter.length !== 0)
+    updatedRecipes = updatedRecipes.filter((recipe) =>
+      ustensilsFilter.some((ustensil) => recipe.ustensils.includes(ustensil))
+    );
+
+  if (ingredientsFilter.length !== 0)
+    updatedRecipes = updatedRecipes.filter((recipe) =>
       ingredientsFilter.some((ingredient) =>
         recipe.ingredients.map((ingredient) => ingredient.ingredient).includes(ingredient)
       )
     );
+
+  return updatedRecipes;
 };
