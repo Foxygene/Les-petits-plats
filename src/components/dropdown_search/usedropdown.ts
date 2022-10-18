6f5031a2dc6eb7c608571847dad87523933e35bc
Foxygene <foxygene.dev@gmail.com>
2022-10-18 23:50:52 +0200

    feat(dropdown search): live update of option on inputs

--- src/components/dropdown_search/usedropdown.ts
+++ src/components/dropdown_search/usedropdown.ts
@@ -1,9 +1,18 @@
+import { gatherFilter } from '../../scripts/gatherfilter';
 import { refreshDOMelementsToFilter } from '../../scripts/refreshdomelementstofilter';
 import { filterPill } from '../filter_pill/filter_pill';
 import { usePill } from '../filter_pill/use_pill';
 
 export const useDropdown = (filterBox: HTMLDivElement): void => {
+  const searchBar = document.querySelector('.dropdown_search');
   const options = document.querySelectorAll('.option');
+
+  searchBar?.addEventListener('input', (event) => {
+    const optionContainer = event.path[1].nextElementSibling;
+
+    optionContainer.innerHTML = console.log(event.srcElement.value);
+    console.log(event.path[1].nextElementSibling);
+  });
 
   options.forEach((option) => {
     option.addEventListener('click', () => {
