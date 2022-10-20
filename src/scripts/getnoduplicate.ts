87caaa5f4dca29d67af2504a80e08ea1252f276b
Foxygene <foxygene.dev@gmail.com>
2022-10-20 19:01:41 +0200

    chore(getnoduplicate): fixed typo

--- src/scripts/getnoduplicate.ts
+++ src/scripts/getnoduplicate.ts
@@ -4,35 +4,36 @@ import type { Recipe } from '../type/recipes';
 export const getNoDuplicate = (type: string, recipes: Recipe[]): Ingredient[] => {
   const noDuplicate: Array<string> = [];
   if (type === 'ingredient') {
-  recipes.forEach((recipe) => {
-    recipe.ingredients.filter((element) => {
-      const isDuplicate = noDuplicate.includes(element.ingredient);
+    recipes.forEach((recipe) => {
+      recipe.ingredients.filter((element) => {
+        const isDuplicate = noDuplicate.includes(element.ingredient);
 
-      if (!isDuplicate) {
-        noDuplicate.push(element.ingredient);
+        if (!isDuplicate) {
+          noDuplicate.push(element.ingredient);
 
-        return true;
-      }
-      return false;
+          return true;
+        }
+        return false;
+      });
     });
-  });}
+  }
 
   if (type === 'appliance') {
     recipes.forEach((recipes) => {
       const isDuplicate = noDuplicate.includes(recipes.appliance);
 
       if (!isDuplicate) noDuplicate.push(recipes.appliance);
-    })
+    });
   }
 
   if (type === 'ustensils') {
     recipes.forEach((recipe) => {
       recipe.ustensils.filter((element) => {
         const isDuplicate = noDuplicate.includes(element);
-  
+
         if (!isDuplicate) {
           noDuplicate.push(element);
-  
+
           return true;
         }
         return false;
