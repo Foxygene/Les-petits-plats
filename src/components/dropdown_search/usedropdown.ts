a6ff941b552314f68639493779a89d1f3c595e27
Foxygene <foxygene.dev@gmail.com>
2022-10-21 19:53:58 +0200

    feat(Dropdown options): Live update on search WIP

--- src/components/dropdown_search/usedropdown.ts
+++ src/components/dropdown_search/usedropdown.ts
@@ -1,18 +1,32 @@
 import { gatherFilter } from '../../scripts/gatherfilter';
+import { filterRecipes } from '../../scripts/getfiltereddata';
+import { getNoDuplicate } from '../../scripts/getnoduplicate';
 import { refreshDOMelementsToFilter } from '../../scripts/refreshdomelementstofilter';
 import { filterPill } from '../filter_pill/filter_pill';
 import { usePill } from '../filter_pill/use_pill';
 
 export const useDropdown = (filterBox: HTMLDivElement): void => {
-  const searchBar = document.querySelector('.dropdown_search');
+  const searchBar = document.querySelectorAll('.dropdown_search');
   const options = document.querySelectorAll('.option');
 
-  searchBar?.addEventListener('input', (event) => {
-    const optionContainer = event.path[1].nextElementSibling;
+  searchBar.forEach((searchBar) =>
+    searchBar!.addEventListener('input', (event) => {
+      const activeFilters = gatherFilter();
+      const filteredData = filterRecipes(activeFilters);
+      const optionContainer = event.path[1].nextElementSibling;
 
-    optionContainer.innerHTML = console.log(event.srcElement.value);
-    console.log(event.path[1].nextElementSibling);
-  });
+      if (event.path[1].classList[1] === 'bg_blue') {
+        const ingredientList = getNoDuplicate('ingredient', filteredData);
+
+        const searchList = ingredientList.filter((option) => event.srcElement.value.includes(option));
+        console.log(searchList);
+
+        optionContainer.innerHTML = searchList;
+      }
+      // console.log(event.path[1].nextElementSibling);
+      // console.log(event.path[1].classList[1]);
+    })
+  );
 
   options.forEach((option) => {
     option.addEventListener('click', () => {
