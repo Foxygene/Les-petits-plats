e276df9b3a63bb3aa386f9716b09a1697a6f3e63
Foxygene <foxygene.dev@gmail.com>
2022-10-28 23:54:31 +0200

    chore(useSearchBox): new design

--- src/components/search_box/usesearchbox.ts
+++ src/components/search_box/usesearchbox.ts
@@ -1,7 +1,9 @@
+import { refreshDOMelementsToFilter } from '../../scripts/refreshdomelementstofilter';
+
 export const useSearchBox = (searchBox: HTMLDivElement): void => {
   const searchInput = searchBox.firstElementChild;
 
   searchInput?.addEventListener('input', (event) => {
-    console.log(event.target.value);
+    refreshDOMelementsToFilter(event.target.value);
   });
 };
